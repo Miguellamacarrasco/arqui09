d5983d0f96c006af438416245f0982911887d286
Miguel Lama <migue.lama@yahoo.com>
2020-07-11 19:31:22 -0500

    virtual insanity

--- makefile.bat
+++ makefile.bat
@@ -1,1 +1,1 @@
-iverilog ALU.v arithmetic.v gates.v logical.v mipsmem.v mipsmulti.v sign_extend.v topmulti.v
+iverilog testbench.v ALU.v arithmetic.v gates.v logical.v mipsmem.v mipsmulti.v sign_extend.v topmulti.v
--- testbench.v
+++ testbench.v
@@ -1,4 +1,27 @@
 `timescale 1ns/1ns
 module tb;
+    reg clk, reset;
+    wire [31:0] writedata, adr;
+    wire memwrite;
+    top tp(clk, reset, writedata, adr, memwrite);
+    always @ (*) begin #1 clk <= !clk; end
+    initial begin
+        $dumpfile("basura.vcd");
+        $dumpvars(0);
+        clk = 0;
+        reset = 1;
+        #1;
+        reset = 0;
+        #1;
+        #1;
+        #1;
+        #1;
+        #1;
+        #1;
+        #1;
+        #1;
+        #1;
 
+
+    end
 endmodule
